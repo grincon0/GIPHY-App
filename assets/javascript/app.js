ed603e60d1af0cab8abed7dffdde804d6e0aebc5
Wake <georgerincon2@gmail.com>
2018-09-25 23:36:01 -0400

    working gif anim toggler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 const strings = ["happy", "sad", "mad", "love", "determined", "worried", "empowered", "aloof", "concentrated", "seriously", "insane"];
 
-
 const renderButtons = (strings) =>  {
     $("#btn-div").empty();
     strings.forEach((term) => {
@@ -10,8 +9,7 @@ const renderButtons = (strings) =>  {
             search: term,
             type: "button",
             text: term
-        });
-      
+        });      
         $("#btn-div").append(btn);
     });
 }
@@ -20,11 +18,9 @@ const getPictures = (caller) => {
 
     const apiKey = "8GVaytATdSSwaU1NWa4z9MoufR2eIw54";
     let userInput = $(caller).attr("search");
-    console.log(caller);
-   
+  
     let search = `${userInput}`;
-    let query = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";
-    
+    let query = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";   
     $.ajax({
         url : query,
         method : "GET"
@@ -35,38 +31,49 @@ const getPictures = (caller) => {
         for(var i = 0; i < source.length; i++){
             let newDiv = $('<div>');
             let header = $(`<h6> Rated : ${source[i].rating}</h6>`);
-            let img = $(`<img id="gif" src=${source[i].images.fixed_height.url} data-still=${source[i].images.fixed_height_still.url}></img>`);
-            
+            let img = $(`<img id=gif src=${source[i].images.fixed_height.url} data-state=anim data-anim=${source[i].images.fixed_height.url} data-still=${source[i].images.fixed_height_still.url}></img>`);
+
             $(newDiv).append(header).append(img);
             $("#img-results").append(newDiv);
         }
+
+        $("#gif").on("click", function () {
+            let gif = $(this);
+            console.log(gif);
+            toggleGif(gif);
+        });
     }).catch(function(err){
         throw err;
     });
 }
 
+const toggleGif = (gif) => {
+    let still = $(gif).attr('data-still');
+    let animated = $(gif).attr('data-anim');
+    let gifState = $(gif).attr('data-state');
+
+    if(gifState === 'anim'){
+        $(gif).attr('src', still);
+        $(gif).attr('data-state', 'still');
+    }else{
+        $(gif).attr('src', animated);
+        $(gif).attr('data-state', 'anim');
+    }
+}
 
 $(document).ready(function () {
     renderButtons(strings);
 
     $(".emotion").on("click", function(){
 
-        console.log(this);
+        
 
         let caller = $(this);
         getPictures(caller);
     });
 
-    //$("#submit-query").on("click", function () {
-      //  let input = $(this).text();
-
-        //$("#img-results").empty();
-        
-      //  getPictures(input);
-      //  console.log('ye');
-
-  //  });
-
-
+ 
 });
+
+
 
